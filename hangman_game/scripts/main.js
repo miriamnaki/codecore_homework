74cf1ed724269a3e5f51e4e0d1369d1cff239ecb
MIRIAM NAKI <nakiyingim@gmail.com>
2021-02-15 09:17:24 -0800

    added main function and sound assets

--- hangman_game/scripts/main.js
+++ hangman_game/scripts/main.js
@@ -24,7 +24,7 @@ $(document).ready(()=>{
         guessedWord.push('__')
       }
       $('.new-word').replaceWith(
-        `<div class='new-word'><h1>${guessedWord.join(' ')}</h1></div>`
+        `<div class='new-word'><h1>${wordToBeGuessed.join(' ')}</h1></div>`
       );
     }
 
@@ -63,12 +63,74 @@ $(document).ready(()=>{
 
     function checkWord(){
       for(let x = 0; x< word.length; x++){
-        for(let y = 0; y < guessedWord; y++){
-          if(word[x] == guessedWord[y]){
-            wordToBeGuessed[x] == word[x]
+        for(let y = 0; y < wordToBeGuessed; y++){
+          if(word[x] == wordToBeGuessed[y]){
+            guessedWord[x] == word[x]
           }
         }
       }
     }
+
+    let failureSound = new Audio('./assets/sounds/failure.wav');
+    let victorySound = new Audio('./assets/sounds/victory.wav');
+
+    function playGame(letter){
+      if(numError > 5){
+        failureSound.play();
+        if(comfirm('You lost! Do you want to play again?')){
+          choseWord()
+          return;
+        }
+        else if(word.join('') == wordToBeGuessed.join('')){
+          if(confirm('You win! Do you wanna play again?')){
+            choseWord()
+            return;
+          }
+        }
+        else{
+          if(wordToBeGuessed.includes(letter)){
+            return; 
+          }
+          wordToBeGuessed.push(letter)
+          if(word.includes(letter)){
+            checkWord()
+          }
+          else{
+            numError++
+          }
+          loadBodyParts()
+        }
+
+        if(word.join('') == guessedWord('')){
+          victorySound.play()
+          return;
+        }
+
+        if(numError > 5){
+          failureSound.play()
+          choseWord()
+        }
+        else{
+          return;
+        }
+
+      }
+
+    }
+    $(".key").on("click", function(event) {
+      $(this).addClass("highlight");
+      let char = event.target.innerHTML.toLocaleLowerCase();
+      playGame(char)
+    })
+
+    document.addEventListener("keydown", event => {
+      const { key } = event;
+      console.log("key:", key);
+      capitalKey = key.toUpperCase();
+      $(`span:contains(${capitalKey})`).addClass("highlight");
+      playGame(key);
+    });
+  
+
 
 })
